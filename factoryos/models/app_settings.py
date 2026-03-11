f2eb1a4b0ca0eca74f31844acfd651ab7bebc231
spiefer87-del <spiefer87@googlemail.com>
2026-03-11 22:05:34 +0100

    Add AppSettings model with company details

--- factoryos/models/app_settings.py
+++ factoryos/models/app_settings.py
@@ -1,1 +1,9 @@
+class AppSettings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
 
+    company_name = db.Column(db.String(200), nullable=True)
+    company_address = db.Column(db.String(400), nullable=True)
+
+    logo_filename = db.Column(db.String(200), nullable=True)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
