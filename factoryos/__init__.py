e4cc0cc09fb10ca3ad7695974dfcd8ba3e5ddeca
spiefer87-del <spiefer87@googlemail.com>
2026-03-11 21:31:57 +0100

    Refactor app initialization and blueprint loading

--- factoryos/__init__.py
+++ factoryos/__init__.py
@@ -1,19 +1,32 @@
 from flask import Flask
-from config import Config
-from extensions import db, login_manager, migrate
-from factoryos.core.blueprint_loader import register_blueprints
+
+from .config import Config
+from .extensions import db, login_manager
+
+from .core.blueprint_loader import load_blueprints
 
 
 def create_app():
 
     app = Flask(__name__)
+
+    # ------------------------
+    # CONFIG
+    # ------------------------
+
     app.config.from_object(Config)
+
+    # ------------------------
+    # EXTENSIONS
+    # ------------------------
 
     db.init_app(app)
     login_manager.init_app(app)
-    migrate.init_app(app, db)
 
-    # 🔥 automatische Blueprint Registrierung
-    register_blueprints(app)
+    # ------------------------
+    # BLUEPRINT AUTO LOADER
+    # ------------------------
+
+    load_blueprints(app)
 
     return app
