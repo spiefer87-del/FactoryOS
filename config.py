23f1d3bb9c205bda07f888a7f8674d69fab3e0d0
spiefer87-del <spiefer87@googlemail.com>
2026-03-11 22:04:10 +0100

    Refactor config.py to include upload paths

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+UPLOAD_DRAWINGS = os.path.join(BASE_DIR, "static/qm_drawings")
+UPLOAD_SNIPPETS = os.path.join(BASE_DIR, "static/qm_snippets")
+UPLOAD_IDENTIFICATION = os.path.join(BASE_DIR, "static/qm_identification")
+
 class Config:
 
     SECRET_KEY = "dev"
@@ -5,3 +13,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///factoryos.db"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
