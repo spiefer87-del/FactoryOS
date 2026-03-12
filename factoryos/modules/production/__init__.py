aaa5aefefe6a7022567c812ce49720ecce005212
spiefer87-del <spiefer87@googlemail.com>
2026-03-12 21:40:57 +0100

    Update __init__.py

--- factoryos/modules/production/__init__.py
+++ factoryos/modules/production/__init__.py
@@ -1,1 +1,11 @@
-from .routes import blueprints
+from .dashboard_routes import bp as dashboard_bp
+from .booking_routes import bp as booking_bp
+from .machine_routes import bp as machine_bp
+from .order_routes import bp as order_bp
+
+blueprints = [
+    dashboard_bp,
+    booking_bp,
+    machine_bp,
+    order_bp
+]
