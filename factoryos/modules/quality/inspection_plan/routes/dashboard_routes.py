f2b3f15a7b12090fc70c62550d3766721a3d40a1
spiefer87-del <spiefer87@googlemail.com>
2026-03-13 09:56:58 +0100

    Update dashboard_routes.py

--- factoryos/modules/quality/inspection_plan/routes/dashboard_routes.py
+++ factoryos/modules/quality/inspection_plan/routes/dashboard_routes.py
@@ -1,1 +1,10 @@
+from flask import render_template
+from flask_login import login_required
 
+from . import bp
+
+
+@bp.route("/")
+@login_required
+def quality_dashboard():
+    return render_template("quality/dashboard.html")
