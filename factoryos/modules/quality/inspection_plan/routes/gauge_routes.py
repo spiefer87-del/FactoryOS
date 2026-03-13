4b7e6c21ad6a7ee6c874b5bb965ffce5431432b6
spiefer87-del <spiefer87@googlemail.com>
2026-03-13 09:26:35 +0100

    Update gauge_routes.py

--- factoryos/modules/quality/inspection_plan/routes/gauge_routes.py
+++ factoryos/modules/quality/inspection_plan/routes/gauge_routes.py
@@ -1,1 +1,63 @@
+from flask import request, redirect
+from flask_login import login_required
 
+from factoryos.extensions import db
+
+from factoryos.modules.quality.inspection_plan.models import (
+    QualityInspectionSection,
+    QualityInspectionGaugeCheck
+)
+
+from factoryos.modules.quality.inspection_plan.change_log_service import (
+    log_change
+)
+
+from . import bp
+
+
+# --------------------------------------------------
+# Add Gauge Check
+# --------------------------------------------------
+
+@bp.route("/section/<int:section_id>/add_gauge_check", methods=["POST"])
+@login_required
+def quality_add_gauge_check(section_id):
+
+    section = QualityInspectionSection.query.get_or_404(section_id)
+
+    if section.version.status != "draft":
+        return redirect(request.referrer)
+
+    last = (
+        QualityInspectionGaugeCheck.query
+        .filter_by(section_id=section.id)
+        .order_by(QualityInspectionGaugeCheck.sort_order.desc())
+        .first()
+    )
+
+    order = 1
+
+    if last:
+        order = last.sort_order + 1
+
+    check = QualityInspectionGaugeCheck(
+        section_id=section.id,
+        name=request.form.get("name"),
+        gauge_id=request.form.get("gauge_id"),
+        method=request.form.get("method"),
+        sort_order=order
+    )
+
+    db.session.add(check)
+
+    section.version.is_dirty = True
+
+    log_change(
+        section.version,
+        "ADD_GAUGE_CHECK",
+        f"Lehrenprüfung '{check.name}' hinzugefügt"
+    )
+
+    db.session.commit()
+
+    return redirect(request.referrer)
