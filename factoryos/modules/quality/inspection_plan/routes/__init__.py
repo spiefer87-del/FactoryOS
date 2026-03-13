c4408bb0c8b1a1c6e793257eb4c8334ce8eecd15
spiefer87-del <spiefer87@googlemail.com>
2026-03-13 08:51:08 +0100

    Update __init__.py

--- factoryos/modules/quality/inspection_plan/routes/__init__.py
+++ factoryos/modules/quality/inspection_plan/routes/__init__.py
@@ -1,1 +1,18 @@
+from flask import Blueprint
 
+bp = Blueprint(
+    "inspection",
+    __name__,
+    url_prefix="/quality/inspection-plans"
+)
+
+# Route-Dateien laden
+from .dashboard_routes import *
+from .plan_routes import *
+from .version_routes import *
+from .section_routes import *
+from .characteristic_routes import *
+from .image_routes import *
+from .drawing_routes import *
+from .gauge_routes import *
+from .pdf_routes import *
