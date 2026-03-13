d2fe5297a2a2c64f8f0e7da97176934f7a7e1056
spiefer87-del <spiefer87@googlemail.com>
2026-03-13 09:05:15 +0100

    Update pdf_routes.py

--- factoryos/modules/quality/inspection_plan/routes/pdf_routes.py
+++ factoryos/modules/quality/inspection_plan/routes/pdf_routes.py
@@ -1,1 +1,29 @@
+from flask import redirect, url_for
+from flask_login import login_required
 
+from factoryos.extensions import db
+from factoryos.modules.quality.inspection_plan.models import QualityInspectionPlanVersion
+from factoryos.modules.quality.inspection_plan.pdf_service import generate_inspection_plan_pdf
+
+from . import bp
+
+
+@bp.route("/export_pdf/<int:version_id>")
+@login_required
+def quality_export_pdf(version_id):
+
+    pdf_path = generate_inspection_plan_pdf(version_id)
+
+    version = QualityInspectionPlanVersion.query.get_or_404(version_id)
+
+    version.pdf_path = pdf_path
+
+    db.session.commit()
+
+    return redirect(
+        url_for(
+            "inspection.quality_version_edit",
+            plan_id=version.plan_id,
+            version_id=version.id
+        )
+    )
