2315bcd4a6be2760180316d7d0d6b57e4da9cb2f
spiefer87-del <spiefer87@googlemail.com>
2026-03-13 15:15:34 +0100

    Update machine_queries.py

--- factoryos/modules/setup/machines/queries/machine_queries.py
+++ factoryos/modules/setup/machines/queries/machine_queries.py
@@ -1,1 +1,45 @@
+from factoryos.extensions import db
+from factoryos.models.machine import Machine
 
+
+def get_all_machines():
+    return (
+        Machine.query
+        .order_by(Machine.name.asc())
+        .all()
+    )
+
+
+def get_machine(machine_id):
+    return Machine.query.get_or_404(machine_id)
+
+
+def create_machine(data):
+
+    machine = Machine(
+        name=data.get("name"),
+        location=data.get("location"),
+        active=True
+    )
+
+    db.session.add(machine)
+    db.session.commit()
+
+    return machine
+
+
+def update_machine(machine, data):
+
+    machine.name = data.get("name")
+    machine.location = data.get("location")
+    machine.active = data.get("active") == "on"
+
+    db.session.commit()
+
+    return machine
+
+
+def delete_machine(machine):
+
+    db.session.delete(machine)
+    db.session.commit()
