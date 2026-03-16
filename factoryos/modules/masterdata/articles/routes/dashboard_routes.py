c133814a19c802bb678f02183869444f4c56c3ad
spiefer87-del <spiefer87@googlemail.com>
2026-03-16 22:22:26 +0100

    Update dashboard_routes.py

--- factoryos/modules/masterdata/articles/routes/dashboard_routes.py
+++ factoryos/modules/masterdata/articles/routes/dashboard_routes.py
@@ -1,13 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
 
-bp = Blueprint(
-    "articles",
-    __name__,
-    url_prefix="/masterdata/articles"
-)
-
-
 @bp.route("/")
 @login_required
 def dashboard():
