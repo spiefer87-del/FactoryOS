5c38c5ef8a94d236902c9f93228861360c3a553c
spiefer87-del <spiefer87@googlemail.com>
2026-03-16 23:13:21 +0100

    Update article_service.py

--- factoryos/modules/masterdata/articles/services/article_service.py
+++ factoryos/modules/masterdata/articles/services/article_service.py
@@ -1,1 +1,35 @@
+from factoryos.extensions import db
+from ..models import Article
 
+
+def create_article(form):
+
+    article = Article(
+        article_no=form.get("article_no"),
+        article_name=form.get("article_name"),
+        description=form.get("description"),
+        status=form.get("status")
+    )
+
+    db.session.add(article)
+    db.session.commit()
+
+    return article
+
+
+def update_article(article, form):
+
+    article.article_no = form.get("article_no")
+    article.article_name = form.get("article_name")
+    article.description = form.get("description")
+    article.status = form.get("status")
+
+    db.session.commit()
+
+    return article
+
+
+def delete_article(article):
+
+    db.session.delete(article)
+    db.session.commit()
