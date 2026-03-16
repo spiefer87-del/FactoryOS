f90d2a19eec28ba3b48eeafce5087570180b2b73
spiefer87-del <spiefer87@googlemail.com>
2026-03-16 21:12:24 +0100

    Create list_routes.py

---
+++ factoryos/modules/masterdata/articles/routes/list_routes.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from flask_login import login_required
+from . import bp
+from factoryos.modules.masterdata.articles.models import Article
+
+
+@bp.route("/list")
+@login_required
+def list_articles():
+
+    articles = Article.query.order_by(Article.article_no).all()
+
+    return render_template(
+        "masterdata/articles/articles_list.html",
+        articles=articles
+    )
