7545a1002241236e47ed59770e4de18317cf8e69
spiefer87-del <spiefer87@googlemail.com>
2026-03-16 22:42:17 +0100

    Update create_routes.py

--- factoryos/modules/masterdata/articles/routes/create_routes.py
+++ factoryos/modules/masterdata/articles/routes/create_routes.py
@@ -1,1 +1,9 @@
+from flask import render_template
+from flask_login import login_required
+from . import bp
 
+
+@bp.route("/create")
+@login_required
+def create():
+    return render_template("masterdata/articles/create.html")
