3077c9d4bb8dc57759577515d1e017a7f169b70a
spiefer87-del <spiefer87@googlemail.com>
2026-03-16 23:19:16 +0100

    Create article_queries.py

---
+++ factoryos/modules/masterdata/articles/queries/article_queries.py
@@ -0,0 +1,36 @@
+from ..models import Article
+
+
+def get_articles():
+
+    return (
+        Article.query
+        .order_by(Article.article_no)
+        .all()
+    )
+
+
+def get_article(article_id):
+
+    return Article.query.get_or_404(article_id)
+
+
+def search_articles(search=None):
+
+    query = Article.query
+
+    if search:
+        query = query.filter(
+            Article.article_no.contains(search) |
+            Article.article_name.contains(search)
+        )
+
+    return query.order_by(Article.article_no).all()
+
+
+def get_statuses():
+
+    return [
+        "active",
+        "inactive"
+    ]
