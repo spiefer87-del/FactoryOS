3e216b6344b5fa5f49ead7c712362ca478d03c11
spiefer87-del <spiefer87@googlemail.com>
2026-03-16 23:21:23 +0100

    Update delete_routes.py

--- factoryos/modules/masterdata/articles/routes/delete_routes.py
+++ factoryos/modules/masterdata/articles/routes/delete_routes.py
@@ -1,1 +1,17 @@
+from flask import redirect, url_for
+from flask_login import login_required
 
+from . import bp
+from ..queries.article_queries import get_article
+from ..services.article_service import delete_article
+
+
+@bp.route("/delete/<int:article_id>")
+@login_required
+def delete(article_id):
+
+    article = get_article(article_id)
+
+    delete_article(article)
+
+    return redirect(url_for("articles.list"))
