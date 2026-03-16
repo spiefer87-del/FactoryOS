639f25c6068751093f82fd5086a90b5c16103fe9
spiefer87-del <spiefer87@googlemail.com>
2026-03-16 21:39:32 +0100

    Update __init__.py

--- factoryos/modules/masterdata/articles/routes/__init__.py
+++ factoryos/modules/masterdata/articles/routes/__init__.py
@@ -7,8 +7,5 @@ bp = Blueprint(
 )
 
 from . import dashboard_routes
-from . import overview_routes
-from . import detail_routes
-from . import create_routes
-from . import edit_routes
-from . import delete_routes
+from . import list_routes
+
