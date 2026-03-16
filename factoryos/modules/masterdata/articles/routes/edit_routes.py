6c4b7cf663505a73c6ae7b521c5e28f283322bdc
spiefer87-del <spiefer87@googlemail.com>
2026-03-16 23:17:50 +0100

    Update edit_routes.py

--- factoryos/modules/masterdata/articles/routes/edit_routes.py
+++ factoryos/modules/masterdata/articles/routes/edit_routes.py
@@ -1,1 +1,24 @@
+from flask import render_template, request, redirect, url_for
+from flask_login import login_required
 
+from . import bp
+from ..queries.article_queries import get_article
+from ..services.article_service import update_article
+
+
+@bp.route("/edit/<int:article_id>", methods=["GET","POST"])
+@login_required
+def edit(article_id):
+
+    article = get_article(article_id)
+
+    if request.method == "POST":
+
+        update_article(article, request.form)
+
+        return redirect(url_for("articles.list"))
+
+    return render_template(
+        "masterdata/articles/edit.html",
+        article=article
+    )
