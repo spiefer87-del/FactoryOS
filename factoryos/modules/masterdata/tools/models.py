7ef880eab7fc5d4600e3f4305bd95ca01319cc41
spiefer87-del <spiefer87@googlemail.com>
2026-03-17 11:12:30 +0100

    Update models.py

--- factoryos/modules/masterdata/tools/models.py
+++ factoryos/modules/masterdata/tools/models.py
@@ -1,1 +1,18 @@
+class ToolError(db.Model):
 
+    __tablename__ = "tool_errors"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    tool_id = db.Column(db.Integer, db.ForeignKey("tools.id"))
+    order_id = db.Column(db.Integer)
+
+    machine_id = db.Column(db.Integer)
+
+    error_type = db.Column(db.String(100))
+
+    description = db.Column(db.Text)
+
+    reported_by = db.Column(db.Integer)
+
+    created_at = db.Column(db.DateTime)
