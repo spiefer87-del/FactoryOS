ff6086cff220945dc56f481dd8aea8b9cc70c124
spiefer87-del <spiefer87@googlemail.com>
2026-03-17 10:27:54 +0100

    Update tool_error_queries.py

--- factoryos/modules/production/queries/tool_error_queries.py
+++ factoryos/modules/production/queries/tool_error_queries.py
@@ -1,1 +1,15 @@
+from ..models import ToolError
 
+
+def get_tool_errors():
+
+    return (
+        ToolError.query
+        .order_by(ToolError.created_at.desc())
+        .all()
+    )
+
+
+def get_tool_error(error_id):
+
+    return ToolError.query.get_or_404(error_id)
