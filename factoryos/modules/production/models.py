b2518ecad9f3f4e6aedbb94485bda23a76c057f2
spiefer87-del <spiefer87@googlemail.com>
2026-03-17 11:14:08 +0100

    Update models.py

--- factoryos/modules/production/models.py
+++ factoryos/modules/production/models.py
@@ -63,4 +63,22 @@ class QuantityReport(db.Model):
     order = db.relationship("Order", backref="qty_reports")
     machine = db.relationship("Machine", backref="qty_reports")
 
+class ToolError(db.Model):
+
+    __tablename__ = "tool_errors"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    tool_id = db.Column(db.Integer, db.ForeignKey("tools.id"))
+    order_id = db.Column(db.Integer)
+
+    machine_id = db.Column(db.Integer)
+
+    error_type = db.Column(db.String(100))
+
+    description = db.Column(db.Text)
+
+    reported_by = db.Column(db.Integer)
+
+    created_at = db.Column(db.DateTime)
 
