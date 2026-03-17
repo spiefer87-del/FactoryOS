31a271e812192b4716127881feecc50f5f6376b5
spiefer87-del <spiefer87@googlemail.com>
2026-03-17 17:00:51 +0100

    Update models.py

--- factoryos/modules/masterdata/tools/models.py
+++ factoryos/modules/masterdata/tools/models.py
@@ -1,10 +1,15 @@
 from factoryos.extensions import db
 
-class Tool(db.Model):
 
+class Tool(db.Model):
     __tablename__ = "tools"
 
     id = db.Column(db.Integer, primary_key=True)
-    tool_no = db.Column(db.String(50))
-    article_no = db.Column(db.String(50))
-    article_name = db.Column(db.String(100))
+
+    tool_no = db.Column(db.String(50), unique=True, nullable=False)
+    name = db.Column(db.String(100))
+    description = db.Column(db.Text)
+
+    created_by_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    created_by = db.relationship("User", foreign_keys=[created_by_id])
