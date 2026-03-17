b50dd0a366c6a32b4c24bf1ce2ac66420ea0881f
spiefer87-del <spiefer87@googlemail.com>
2026-03-17 08:33:40 +0100

    Update detail_routes.py

--- factoryos/modules/masterdata/articles/routes/detail_routes.py
+++ factoryos/modules/masterdata/articles/routes/detail_routes.py
@@ -1,1 +1,17 @@
+from flask import render_template
+from flask_login import login_required
 
+from . import bp
+from ..queries.article_queries import get_article
+
+
+@bp.route("/<int:article_id>")
+@login_required
+def detail(article_id):
+
+    article = get_article(article_id)
+
+    return render_template(
+        "masterdata/articles/detail.html",
+        article=article
+    )
