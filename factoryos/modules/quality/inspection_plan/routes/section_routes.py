b0df70e395d41a96a116162351755c100d4584d5
spiefer87-del <spiefer87@googlemail.com>
2026-03-18 20:56:59 +0100

    Update section_routes.py

--- factoryos/modules/quality/inspection_plan/routes/section_routes.py
+++ factoryos/modules/quality/inspection_plan/routes/section_routes.py
@@ -34,4 +34,18 @@ def quality_add_characteristic(section_id):
 
     add_characteristic(section, request.form)
 
-    return redirect(request.referrer or url_for("inspection.quality_inspection_plan"))
+   @bp.route("/section/<int:section_id>/add_characteristic", methods=["POST"])
+@login_required
+def quality_add_characteristic(section_id):
+
+    section = QualityInspectionSection.query.get_or_404(section_id)
+
+    add_characteristic(section, request.form)
+
+    return redirect(
+        url_for(
+            "inspection.quality_version_edit",
+            plan_id=section.version.plan_id,
+            version_id=section.version_id
+        )
+    )
