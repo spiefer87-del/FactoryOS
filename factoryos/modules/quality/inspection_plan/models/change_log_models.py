42a427a0a2fcfac3066de140d065a09dcb26a010
spiefer87-del <spiefer87@googlemail.com>
2026-03-18 16:21:08 +0100

    Update change_log_models.py

--- factoryos/modules/quality/inspection_plan/models/change_log_models.py
+++ factoryos/modules/quality/inspection_plan/models/change_log_models.py
@@ -1,1 +1,25 @@
+class QualityInspectionChangeLog(db.Model):
 
+    __tablename__ = "qm_change_logs"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    plan_id = db.Column(
+        db.Integer,
+        db.ForeignKey("qm_plans.id", ondelete="CASCADE")
+    )
+
+    version_id = db.Column(
+        db.Integer,
+        db.ForeignKey("qm_plan_versions.id", ondelete="CASCADE")
+    )
+
+    action = db.Column(db.String(50))
+    message = db.Column(db.String(500))
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    user = db.relationship("User")
+    version = db.relationship("QualityInspectionPlanVersion")
