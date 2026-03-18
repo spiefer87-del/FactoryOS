bcc64a180f0368ed421053d4d23fc3b2f08c3cf8
spiefer87-del <spiefer87@googlemail.com>
2026-03-18 16:54:43 +0100

    Update version_routes.py

--- factoryos/modules/quality/inspection_plan/routes/version_routes.py
+++ factoryos/modules/quality/inspection_plan/routes/version_routes.py
@@ -6,15 +6,14 @@ from sqlalchemy.orm import selectinload
 
 from factoryos.extensions import db
 
-from factoryos.modules.quality.gauges.models import Gauge
-
 from factoryos.modules.quality.inspection_plan.services.section_service import add_section
 from factoryos.modules.quality.inspection_plan.services.change_log_service import log_change
 from factoryos.modules.quality.inspection_plan.services.revision_service import create_new_revision
 
-from factoryos.modules.quality.inspection_plan.models import (
+from ..models import (
     QualityInspectionSection,
-    QualityInspectionCharacteristic
+    QualityInspectionCharacteristic,
+    Gauge
 )
 
 from . import bp
