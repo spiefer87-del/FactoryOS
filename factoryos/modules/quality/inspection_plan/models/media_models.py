1a7703a188342e561c73d4399ff2b16840dc0be8
spiefer87-del <spiefer87@googlemail.com>
2026-03-18 16:20:32 +0100

    Update media_models.py

--- factoryos/modules/quality/inspection_plan/models/media_models.py
+++ factoryos/modules/quality/inspection_plan/models/media_models.py
@@ -1,1 +1,23 @@
+class QualityInspectionIdentificationImage(db.Model):
+    __tablename__ = "qm_section_images"
 
+    id = db.Column(db.Integer, primary_key=True)
+
+    section_id = db.Column(
+        db.Integer,
+        db.ForeignKey("qm_sections.id"),
+        nullable=False
+    )
+
+    image_path = db.Column(db.String(300), nullable=False)
+
+    description = db.Column(db.String(300))
+
+    uploaded_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    uploaded_by_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    section = db.relationship(
+        "QualityInspectionSection",
+        back_populates="images"
+    )
