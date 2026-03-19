9d180d28894036b545a5dc80af3945ed102cc8d9
spiefer87-del <spiefer87@googlemail.com>
2026-03-19 20:59:16 +0100

    Update pdf_routes.py

--- factoryos/modules/quality/inspection_plan/routes/pdf_routes.py
+++ factoryos/modules/quality/inspection_plan/routes/pdf_routes.py
@@ -24,6 +24,6 @@ def quality_export_pdf(version_id):
         url_for(
             "inspection.quality_version_edit",
             plan_id=version.plan_id,
-            version_id=version.id
+            plan_version_id=version.id
         )
     )
