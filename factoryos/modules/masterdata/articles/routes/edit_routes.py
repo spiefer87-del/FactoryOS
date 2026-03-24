4a799993c57fea8b9bce3f0ca9c7d5bd43a6f4f2
spiefer87-del <spiefer87@googlemail.com>
2026-03-24 09:00:00 +0100

    Update edit_routes.py

--- factoryos/modules/masterdata/articles/routes/edit_routes.py
+++ factoryos/modules/masterdata/articles/routes/edit_routes.py
@@ -5,20 +5,22 @@ from . import bp
 from ..queries.article_queries import get_article
 from ..services.article_service import update_article
 
-
 @bp.route("/edit/<int:article_id>", methods=["GET","POST"])
 @login_required
 def edit(article_id):
 
-    article = get_article(article_id)
+    article = Article.query.get_or_404(article_id)
 
     if request.method == "POST":
-
         update_article(article, request.form)
-
         return redirect(url_for("articles.list"))
+
+    tools = Tool.query.order_by(Tool.tool_no).all()
 
     return render_template(
         "masterdata/articles/edit.html",
-        article=article
+        article=article,
+        tools=tools
     )
+    
+
