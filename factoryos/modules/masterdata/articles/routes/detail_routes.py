51cfe001def5353f2f7be64fa86501bbd4cabc2b
spiefer87-del <spiefer87@googlemail.com>
2026-03-26 10:02:42 +0100

    Update detail_routes.py

--- factoryos/modules/masterdata/articles/routes/detail_routes.py
+++ factoryos/modules/masterdata/articles/routes/detail_routes.py
@@ -2,7 +2,7 @@ from flask import render_template
 from flask_login import login_required
 
 from . import bp
-from ..queries.article_queries import get_article
+from ..queries.article_queries import get_article, get_article_logs
 
 
 @bp.route("/<int:article_id>")
@@ -10,8 +10,10 @@ from ..queries.article_queries import get_article
 def detail(article_id):
 
     article = get_article(article_id)
+    logs = get_article_logs(article.id)  # 🔥 jetzt aus Query
 
     return render_template(
         "masterdata/articles/detail.html",
-        article=article
+        article=article,
+        logs=logs
     )
