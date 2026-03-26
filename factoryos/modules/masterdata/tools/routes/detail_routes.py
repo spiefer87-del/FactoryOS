13de6500e3f6a4fb64985e51100f963a3da9406c
spiefer87-del <spiefer87@googlemail.com>
2026-03-26 19:07:12 +0100

    Update detail_routes.py

--- factoryos/modules/masterdata/tools/routes/detail_routes.py
+++ factoryos/modules/masterdata/tools/routes/detail_routes.py
@@ -3,15 +3,17 @@ from flask_login import login_required
 
 from . import bp
 from factoryos.modules.masterdata.tools.models import Tool
-
+from factoryos.modules.masterdata.tools.queries.tool_queries import get_tool_logs
 
 @bp.route("/<int:tool_id>")
 @login_required
 def detail(tool_id):
 
     tool = Tool.query.get_or_404(tool_id)
+    logs = get_tool_logs(tool_id)
 
     return render_template(
         "masterdata/tools/detail.html",
-        tool=tool
+        tool=tool,
+        logs=logs   # 🔥 WICHTIG
     )
