14720ca41b1c7003fa5740933d00daa44c4a4101
spiefer87-del <spiefer87@googlemail.com>
2026-03-26 20:57:45 +0100

    Create activity_routes.py

---
+++ factoryos/modules/activity/routes/activity_routes.py
@@ -0,0 +1,24 @@
+from flask import render_template, request
+from flask_login import login_required
+
+from .. import bp
+from factoryos.core.models.change_log import ChangeLog
+
+
+@bp.route("/")
+@login_required
+def list():
+
+    query = ChangeLog.query
+
+    entity_type = request.args.get("type")
+
+    if entity_type:
+        query = query.filter_by(entity_type=entity_type)
+
+    logs = query.order_by(ChangeLog.created_at.desc()).limit(50).all()
+
+    return render_template(
+        "activity/list.html",
+        logs=logs
+    )
