818b3002ef41640257197bfd25b49e9ba43ec07a
spiefer87-del <spiefer87@googlemail.com>
2026-04-02 10:43:34 +0200

    Update tool_error.js

--- factoryos/static/js/tool_error.js
+++ factoryos/static/js/tool_error.js
@@ -1,3 +1,13 @@
+function generateTempId() {
+    return 'xxxxxxx-xxxx-4xxx-yxxx-xxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+const TEMP_ID = generateTempId();
+
 document.addEventListener("DOMContentLoaded", function () {
 
     // =========================
