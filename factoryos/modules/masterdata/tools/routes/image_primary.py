7c8ca0d331c530d3a70a20cc704e2e7006d3f27b
spiefer87-del <spiefer87@googlemail.com>
2026-04-14 12:00:24 +0200

    Update image_primary.py

--- factoryos/modules/masterdata/tools/routes/image_primary.py
+++ factoryos/modules/masterdata/tools/routes/image_primary.py
@@ -1,1 +1,25 @@
+from flask import redirect, url_for
+from flask_login import login_required
 
+from . import bp
+from factoryos.extensions import db
+from factoryos.modules.masterdata.tools.models import ToolImage
+
+
+@bp.route("/image-primary/<int:image_id>")
+@login_required
+def image_primary(image_id):
+
+    image = ToolImage.query.get_or_404(image_id)
+
+    ToolImage.query.filter_by(
+        tool_id=image.tool_id
+    ).update({"is_primary": False})
+
+    image.is_primary = True
+
+    db.session.commit()
+
+    return redirect(
+        url_for("tools.edit", tool_id=image.tool_id)
+    )
