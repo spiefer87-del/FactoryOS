9be1a16dadd42bc782c4659a7c108b7f7bdcfe00
spiefer87-del <spiefer87@googlemail.com>
2026-06-12 13:26:43 +0200

    Add import_errors route with login requirement

--- factoryos/modules/tool_errors/routes/import_routes.py
+++ factoryos/modules/tool_errors/routes/import_routes.py
@@ -1,1 +1,13 @@
+from flask import render_template
+from flask_login import login_required
 
+from . import bp
+
+
+@bp.route("/import")
+@login_required
+def import_errors():
+
+    return render_template(
+        "tool_errors/import.html"
+    )
