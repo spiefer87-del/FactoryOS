6bf4bed73fb154061cfb62779e2f103903f38426
spiefer87-del <spiefer87@googlemail.com>
2026-06-12 13:46:32 +0200

    Update import_routes.py

--- factoryos/modules/tool_errors/routes/import_routes.py
+++ factoryos/modules/tool_errors/routes/import_routes.py
@@ -1,5 +1,8 @@
 from flask import render_template
 from flask_login import login_required
+from io import BytesIO
+from flask import send_file
+from openpyxl import Workbook
 
 from . import bp
 
@@ -10,4 +13,41 @@ def import_errors():
 
     return render_template(
         "tool_errors/import.html"
+    )
+
+
+@bp.route("/import/template")
+@login_required
+def download_error_import_template():
+
+    wb = Workbook()
+
+    ws = wb.active
+    ws.title = "Tool Errors"
+
+    ws.append([
+        "tool_no",
+        "error_type",
+        "description",
+        "tool_status"
+    ])
+
+    ws.append([
+        "WZ-10001",
+        "Gratbildung",
+        "Grat an der Trennebene",
+        "wartung"
+    ])
+
+    output = BytesIO()
+
+    wb.save(output)
+
+    output.seek(0)
+
+    return send_file(
+        output,
+        as_attachment=True,
+        download_name="ToolError_Import_Vorlage.xlsx",
+        mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
