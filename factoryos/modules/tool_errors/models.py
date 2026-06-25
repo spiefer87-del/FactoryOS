a5d5a34d8a1cb3c78cf1f539cbdaa686a5600d46
spiefer87-del <spiefer87@googlemail.com>
2026-06-25 19:50:47 +0200

    Modify error_no column attributes in tool_errors model
    Updated error_no column to be non-nullable and indexed.

--- factoryos/modules/tool_errors/models.py
+++ factoryos/modules/tool_errors/models.py
@@ -8,8 +8,11 @@ class ToolError(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
 
-    error_no = db.Column(db.String(20), unique=True)
-
+    error_no = db.Column(
+        db.String(20),
+        nullable=False,
+        index=True
+    )
     tool_id = db.Column(db.Integer, db.ForeignKey("tools.id"), nullable=False)
 
     error_type = db.Column(db.String(100))
