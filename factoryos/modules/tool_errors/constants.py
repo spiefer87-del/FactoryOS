bb9f35c2dec9b3bbb7f77ac320c947cbdab6ec4c
spiefer87-del <spiefer87@googlemail.com>
2026-06-25 11:21:19 +0200

    Add workflow status constants and colors

---
+++ factoryos/modules/tool_errors/constants.py
@@ -0,0 +1,13 @@
+WORKFLOW_STATUSES = {
+    "draft": "Entwurf",
+    "review": "In Prüfung",
+    "released": "Freigegeben",
+    "closed": "Geschlossen"
+}
+
+WORKFLOW_STATUS_COLORS = {
+    "draft": "status-draft",
+    "review": "status-review",
+    "released": "status-released",
+    "closed": "status-closed"
+}
