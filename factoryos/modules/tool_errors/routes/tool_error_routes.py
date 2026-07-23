aa077bb5c2caa75e313d0229e813562128568935
spiefer87-del <spiefer87@googlemail.com>
2026-07-23 11:21:09 +0200

    Add permission check for Excel export in dashboard

--- factoryos/modules/tool_errors/routes/tool_error_routes.py
+++ factoryos/modules/tool_errors/routes/tool_error_routes.py
@@ -80,6 +80,10 @@ def dashboard():
     return render_template(
         "tool_errors/dashboard.html",
         errors=errors,
+        can_export_excel=has_permission(
+            current_user,
+            "tool_error.excel_export"
+        ),
         WORKFLOW_STATUSES=WORKFLOW_STATUSES,
         WORKFLOW_STATUS_COLORS=WORKFLOW_STATUS_COLORS
     )
