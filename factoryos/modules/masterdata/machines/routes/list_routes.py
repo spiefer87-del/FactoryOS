0a285f85a260baa3b8f3af0042be0d64971dc83d
spiefer87-del <spiefer87@googlemail.com>
2026-07-27 09:18:09 +0200

    Add route to list machines with permissions

--- factoryos/modules/masterdata/machines/routes/list_routes.py
+++ factoryos/modules/masterdata/machines/routes/list_routes.py
@@ -1,1 +1,49 @@
+from flask import render_template
+from flask_login import login_required, current_user
 
+from factoryos.core.auth import (
+    permission_required,
+    has_permission
+)
+
+from factoryos.modules.masterdata.shared.constants import (
+    MACHINE_TYPES,
+    MACHINE_STATUSES,
+    MACHINE_STATUS_COLORS
+)
+
+from ..queries.machine_queries import get_machines
+
+from . import bp
+
+
+@bp.route("/list")
+@login_required
+@permission_required("machines.view")
+def list_machines():
+
+    machines = get_machines()
+
+    return render_template(
+        "masterdata/machines/list.html",
+        machines=machines,
+
+        MACHINE_TYPES=MACHINE_TYPES,
+        MACHINE_STATUSES=MACHINE_STATUSES,
+        MACHINE_STATUS_COLORS=MACHINE_STATUS_COLORS,
+
+        can_create_machine=has_permission(
+            current_user,
+            "machines.create"
+        ),
+
+        can_edit_machines=has_permission(
+            current_user,
+            "machines.edit"
+        ),
+
+        can_delete_machines=has_permission(
+            current_user,
+            "machines.delete"
+        )
+    )
