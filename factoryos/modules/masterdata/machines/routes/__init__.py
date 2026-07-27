a5486872accb9edf1a1e0301579e8465066f8557
spiefer87-del <spiefer87@googlemail.com>
2026-07-27 09:16:54 +0200

    Initialize Blueprint for machine routes

--- factoryos/modules/masterdata/machines/routes/__init__.py
+++ factoryos/modules/masterdata/machines/routes/__init__.py
@@ -1,1 +1,16 @@
+from flask import Blueprint
 
+
+bp = Blueprint(
+    "machines",
+    __name__,
+    url_prefix="/masterdata/machines"
+)
+
+
+from .dashboard_routes import *
+from .list_routes import *
+from .create_routes import *
+from .edit_routes import *
+from .delete_routes import *
+from .detail_routes import *
